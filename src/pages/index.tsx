b946bf334ccdf8a00c390025c8e4aabef477a4db
pryter <peterphongpak@gmail.com>
2022-03-21 22:31:25 +0700

    Add simple landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,84 @@
 import { useEffect, useState } from "react"
 
-import { PresentationControls, Text } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { Canvas, useFrame } from "@react-three/fiber"
+import Head from "next/head"
+import { MathUtils } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
+const Scene = ({ gltf, title }: any) => {
+  useFrame((state) => {
+    if (!title) {
+      const { camera } = state
+      camera.zoom = MathUtils.lerp(camera.zoom, 0.6, 0.1)
+      camera.updateProjectionMatrix()
+    }
+  })
+
+  return (
+    <>
+      <OrbitControls
+        enablePan={true}
+        enableZoom={true}
+        enableRotate={true}
+        target={title ? [-20, 1.5, -6] : [0, 0, 0]}
+      />
+
+      <>
+        {
+          // @ts-ignore
+          <primitive object={gltf.scene}></primitive>
+        }
+        <pointLight
+          position={[0, 10, 0]}
+          rotation={[0, 0, 45.2]}
+          power={4}
+          distance={30}
+          decay={1.6}
+          color={"#fff"}
+        />
+        <pointLight
+          position={[-4.9, 3.3067, 5.47324]}
+          rotation={[0, 0, 45.2]}
+          power={20}
+          distance={9}
+          decay={1.6}
+          color={"#FFED6D"}
+        />
+        <pointLight
+          position={[7.159, 3.3067, 4.0242]}
+          rotation={[0, 0, 211]}
+          power={20}
+          distance={9}
+          decay={1.6}
+          color={"#FFED6D"}
+        />
+        <rectAreaLight
+          position={[7.85, 20.7331, 13.469]}
+          rotation={[-57, -80.5, -46.7]}
+          power={2000}
+          color="#98BBD8"
+        />
+        <rectAreaLight
+          position={[-10.1477, 17.7331, -15.531]}
+          rotation={[-629, -19.8, -4.61]}
+          power={14000}
+          color="#98BBD8"
+        />
+      </>
+    </>
+  )
+}
 const Index = () => {
   const [gltf, setObj] = useState<any>(null)
+
+  const [title, setTitle] = useState(true)
 
   useEffect(() => {
     const load = async () => {
       const loader = new GLTFLoader()
       const ob = await loader.loadAsync(
-        "https://pryter.me/assets/models/myfirstTree.glb"
+        "https://pryter.me/assets/scenes/forest_scene_web.glb"
       )
       setObj(ob)
     }
@@ -20,43 +87,72 @@ const Index = () => {
   }, [])
 
   return (
-    <div className="flex fixed flex-col justify-center items-center pr-32 w-full min-h-screen bg-blue-100">
-      {gltf === null ? (
-        "Loading"
-      ) : (
-        <Canvas
-          style={{
-            width: "130vw",
-            height: "180vw",
-            maxHeight: "1000px",
+    <>
+      <Head>
+        <title>Pryter</title>
+      </Head>
+      <div className="flex fixed flex-col justify-center items-center w-full min-h-screen bg-gray-900">
+        {[...Array(30)].map((_, i) => {
+          const top = Math.floor(Math.random() * 100)
+          const left = Math.floor(Math.random() * 100)
+
+          return (
+            <div
+              key={`start-${i}`}
+              style={{ top: `${top}vh`, left: `${left}vw` }}
+              className="fixed w-1 h-1 bg-white"
+            />
+          )
+        })}
+        {gltf === null ? (
+          <h1 className="text-xl text-white animate-pulse">Loading..</h1>
+        ) : (
+          <Canvas
+            style={{
+              width: "100vw",
+              height: "100vh",
+              maxHeight: "100vh",
+            }}
+            resize={{ offsetSize: true }}
+            dpr={[1, 2]}
+            camera={{
+              position: [10, 2, 6],
+              fov: 60,
+              zoom: 1.4,
+            }}
+          >
+            <Scene gltf={gltf} title={title} />
+          </Canvas>
+        )}
+      </div>
+      {title && (
+        <div
+          onClick={() => {
+            setTitle(false)
           }}
-          resize={{ offsetSize: true }}
-          dpr={[1, 2]}
-          camera={{ position: [0, 0, 5.5], fov: 60 }}
+          className="flex fixed items-center w-full min-h-screen text-gray-50"
         >
-          <pointLight intensity={1} position={[10, 10, 26]} color="#fff" />
-          <PresentationControls
-            rotation={[0, 0.6, 0]}
-            snap={true}
-            polar={[0, Math.PI / 8]}
-          >
-            {
-              // @ts-ignore
-              <primitive object={gltf.scene}></primitive>
-            }
-          </PresentationControls>
-          <Text
-            scale={[3, 3, 3]}
-            color="black"
-            anchorX="center"
-            anchorY="middle"
-            position={[0.5, -0.6, 0]}
-          >
-            My First Blender Tree
-          </Text>
-        </Canvas>
+          <div className="flex fixed top-0 justify-between items-center py-3 px-6 space-x-2 w-full bg-gray-500 bg-opacity-25 border border-white border-opacity-20 sm:space-x-0 md:px-10 blackdrop-blur-sm">
+            <h1 className="font-semibold">Pryter</h1>
+            <div className="flex items-center space-x-2">
+              <span className="text-sm">Learning Blender: </span>
+              <div className="relative w-32 h-4 bg-white bg-opacity-90 rounded-md">
+                <div className="absolute w-16 h-4 bg-gray-600 bg-opacity-70 rounded-md border border-gray-700 animate-pulse" />
+              </div>
+            </div>
+          </div>
+          <div className="flex relative flex-col justify-center items-center py-6 w-full  md:min-h-screen">
+            <h1 className="text-4xl font-medium">Hello there</h1>
+            <p className="text-lg font-medium">
+              Welcome to my personal website!
+            </p>
+            <div className="absolute py-2 px-4 mt-48 bg-gray-500 bg-opacity-25 rounded-lg border border-white border-opacity-30 backdrop-blur-sm animate-pulse cursor-pointer">
+              <span>Tap anywhere to look around..</span>
+            </div>
+          </div>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
