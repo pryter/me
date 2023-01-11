e9aab125971b1f102b38ca163ec21009b07677e6
pryter <peterphongpak@gmail.com>
2023-01-11 23:46:08 +0700

    fix: blur effect

--- src/component/shared/NavBar.tsx
+++ src/component/shared/NavBar.tsx
@@ -92,7 +92,7 @@ export const NavBar = () => {
       <motion.div
         animate={animation2}
         transition={{ delay: showNav ? 0 : 0.4 }}
-        className="absolute top-0 -right-6 z-[100] w-[200px] h-screen bg-white bg-opacity-20 rounded-l-lg border border-gray-700 backdrop-blur-sm"
+        className="absolute top-0 -right-6 z-[100] w-[200px] h-screen bg-white bg-opacity-20 rounded-l-lg border border-gray-700 bg-blur-sm"
       >
         <motion.div
           animate={animation}
